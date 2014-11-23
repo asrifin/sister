6a3da696946a9fd7cd5f53ffbedac3b30a4eb4ac
asrifin <asrifin_ilham@yahoo.com>
2014-11-23 12:30:53 +0700

    m_periode

--- psb/models/m_periode.php
+++ psb/models/m_periode.php
@@ -182,6 +182,7 @@
 							'kodeawalan' =>$r['kodeawalan'],
 							'angkatan'   =>$r['angkatan'],
 <<<<<<< HEAD
+<<<<<<< HEAD
 							'kapasitas'   =>$r['kapasitas'],
 =======
 							'status'      =>$stat,
@@ -194,6 +195,8 @@
 							'kapasitas'  =>$r['kapasitas'],
 							'keterangan' =>$r['keterangan'],
 >>>>>>> 5e763dc7da1da4a0c012a150dd0f4e990f58d772
+=======
+>>>>>>> parent of eb0a301... PSB V 1.1
 =======
 >>>>>>> parent of eb0a301... PSB V 1.1
 							'keterangan' =>$r['keterangan'],
