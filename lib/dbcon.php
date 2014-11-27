a9c5084d6d615a680f76d53455b991b2924ef2c5
taufikhosting <taufikhosting@gmail.com>
2014-11-27 21:09:41 +0700

    syncronize db sister-siadu

--- lib/dbcon.php
+++ lib/dbcon.php
@@ -3,8 +3,8 @@
 	$server   = "localhost";
 	$username = "root";
 	$password = "";
-	$database = "sisterdb";
-	// $database = "josh_elyon";
+	// $database = "sisterdb";
+	$database = "josh_elyon";
 	mysql_connect($server,$username,$password) or die("Koneksi gagal");
 	mysql_select_db($database) or die("Database tidak bisa dibuka");
 ?>
