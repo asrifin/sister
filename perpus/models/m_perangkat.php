538a1f938a80bdbc2649a4b0529ad5f14424607a
asrifin <asrifin_ilham@yahoo.com>
2015-03-21 16:00:14 +0700

    Perpus V.1.3 katalog

--- perpus/models/m_perangkat.php
+++ perpus/models/m_perangkat.php
@@ -80,79 +80,60 @@
 			// tampil ---------------------------------------------------------------------
 			case 'tampil':
 				switch ($_POST['subaksi']) {
-					case 'ju':
-						$member  = isset($_POST['memberS'])?filter(trim($_POST['memberS'])):'';
-						$barcode = isset($_POST['barcodeS'])?filter(trim($_POST['barcodeS'])):'';
-						$judul   = isset($_POST['judulS'])?filter(trim($_POST['judulS'])):'';
+					case 'id':
+						$sql = 'SELECT nilai
+								FROM pus_setting
+								WHERE 
+									kunci="idfmt"
+								';
+						// print_r($sql);exit();
+						$query = mysql_query($sql);
+						$hasil = mysql_fetch_assoc($query);
 
-						$sql       = 'SELECT * 
-									from '.$tb.' 
-									WHERE 
-										(nomer like "%'.$ju_no.'%" OR nomer like "%'.$ju_no.'%" ) AND
-										uraian like "%'.$ju_uraian.'%"';
-						// print_r($sql);exit(); 	
-						if(isset($_POST['starting'])){ //nilai awal halaman
-							$starting=$_POST['starting'];
-						}else{
-							$starting=0;
-						}
-
-						$recpage = 5;//jumlah data per halaman
-						$aksi    ='tampil';
-						$subaksi ='ju';
-						$obj     = new pagination_class($sql,$starting,$recpage,$aksi,$subaksi);
-						$result  = $obj->result;
-
-						#ada data
-						$jum = mysql_num_rows($result);
-						$out ='';$totaset=0;
-						if($jum!=0){	
-							$nox = $starting+1;
-							while($res = mysql_fetch_array($result)){	
-								$btn ='<td>
-											<button data-hint="ubah"  class="button" onclick="juFR('.$res['replid'].');">
-												<i class="icon-pencil on-left"></i>
-											</button>
-											<button data-hint="hapus"  class="button" onclick="grupDel('.$res['replid'].');">
-												<i class="icon-remove on-left"></i>
-										 </td>';
-								$s2 = 'SELECT r.kode,r.nama,j.debet,j.kredit
-										from keu_jurnal j,keu_rekening r 
-										where 
-											j.transaksi ='.$res['replid'].' AND 
-											j.rek=r.replid
-										ORDER BY kredit  ASC';
-								$e2 = mysql_query($s2);
-								$tb2='';
-								if(mysql_num_rows($e2)!=0){
-	   								$tb2.='<table class="bordered striped lightBlue" width="100%">';
-		   							while($r2=mysql_fetch_assoc($e2)){
-		   								$tb2.='<tr>
-		   										<td>'.$r2['nama'].'</td>
-		   										<td>'.$r2['kode'].'</td>
-		   										<td>Rp. '.number_format($r2['debet']).',-</td>
-		   										<td>Rp. '.number_format($r2['kredit']).',-</td>
-		   									</tr>';
-		   							}$tb2.='</table>';
-								}$out.= '<tr>
-											<td>'.tgl_indo($res['tanggal']).'</td>
-											<td>'.ju_nomor($res['nomer'],$res['jenis'],$res['nobukti']).'</td>
-											<td>'.$res['uraian'].'</td>
-											<td style="display:visible;" class="uraianCOL">'.$tb2.'</td>
-											'.$btn.'
-										</tr>';
-							}
-						}else{ #kosong
-							$out.= '<tr align="center">
-									<td  colspan=10 ><span style="color:red;text-align:center;">
-									... data tidak ditemukan...</span></td></tr>';
-						}
-						#link paging
-						$out.= '<tr class="info"><td colspan=10>'.$obj->anchors.'</td></tr>';
-						$out.='<tr class="info"><td colspan=10>'.$obj->total.'</td></tr>';
+						$out = $hasil['nilai'];
 					break;
-					// grup barang
+
+					case 'barkode':
+						$sql = 'SELECT nilai
+								FROM pus_setting
+								WHERE 
+									kunci="bkfmt"
+								';
+						// print_r($sql);exit();
+						$query = mysql_query($sql);
+						$hasil = mysql_fetch_assoc($query);
+
+						$out = $hasil['nilai'];
+					break;
+
+					case 'judul':
+						$sql = 'SELECT nilai
+								FROM pus_setting
+								WHERE 
+									kunci="labelt"
+								';
+						// print_r($sql);exit();
+						$query = mysql_query($sql);
+						$hasil = mysql_fetch_assoc($query);
+
+						$out = $hasil['nilai'];
+					break;
+
+					case 'deskripsi':
+						$sql = 'SELECT nilai
+								FROM pus_setting
+								WHERE 
+									kunci="labeld"
+								';
+						// print_r($sql);exit();
+						$query = mysql_query($sql);
+						$hasil = mysql_fetch_assoc($query);
+
+						$out = $hasil['nilai'];
+					break;
+										
 				}
+
 			break; 
 			// tampil ---------------------------------------------------------------------
 
@@ -185,95 +166,6 @@
 				}
 			break;
 			// generate kode
-
-			// head info ------------------------------------------------------------------
-			case 'headinfo':
-				switch ($_POST['subaksi']) {
-					case 'katalog':
-						$s = 'SELECT 
-								g.nama as grup,
-								l.nama as lokasi,
-								sum(b.harga)as totaset
-							  FROM 
-							  	'.$tb4.' b,
-							  	'.$tb3.' k,
-							  	'.$tb2.' l,
-							  	'.$tb.' g
-							  WHERE 
-								g.replid  = '.$_POST['grup'].' and 
-								b.katalog = k.replid and
-								g.lokasi  = l.replid and
-								g.replid  = k.grup';
-						$q    = mysql_query($s);
-						$stat = ($q)?'sukses':'gagal';
-						$r    = mysql_fetch_assoc($q);
-						$out  = json_encode(array(
-									'status'  =>$stat,
-									'grup'    =>$r['grup'],
-									'lokasi'  =>$r['lokasi'],
-									'totaset' =>number_format($r['totaset'])
-								));
-					break;
-
-					case 'barang':
-						$s = '	SELECT
-									g.replid,
-									g.nama as grup,(
-										SELECT nama
-										from sar_lokasi 
-										where replid = g.lokasi
-									)as lokasi,
-									IFNULL(tbjum.totbarang,0)totbarang,
-									tbjum.susut,
-									tbjum.nama as katalog,
-									tbjum.totaset,
-									tbjum.photo2
-								from 
-									sar_grup g
-									LEFT JOIN (
-										SELECT 
-											k.replid,
-											k.grup,
-											k.susut,
-											k.nama,
-											k.photo2,
-											count(*)totbarang,
-											sum(b.harga)totaset
-										from 
-											sar_katalog k,
-											sar_barang b
-										WHERE
-											k.replid = b.katalog AND
-											k.replid = '.$_POST['katalog'].'
-									)tbjum on tbjum.grup = g.replid
-								where 
-									tbjum.replid= '.$_POST['katalog'];
-						// var_dump($s);exit();
-						$e = mysql_query($s) or die(mysql_error());
-						$r = mysql_fetch_assoc($e);
-						if(!$e){
-							$stat='gagal';
-						}else{
-							$stat ='sukses';
-							$dt   = array(
-										'idkatalog' =>$r['replid'],
-										'katalog'   =>$r['katalog'],
-										'grup'      =>$r['grup'],
-										'photo2'    =>$r['photo2'],
-										'lokasi'    =>$r['lokasi'],
-										'susut'     =>$r['susut'],
-										'totbarang' =>$r['totbarang'],
-										'totaset'   =>number_format($r['totaset'])
-									);
-						}
-						$out  = json_encode(array(
-									'status' =>$stat,
-									'data'   =>$dt
-								));
-					break;
-				}
-			break;
-			// head info ------------------------------------------------------------------
 
 			// add / edit -----------------------------------------------------------------
 			case 'simpan':
@@ -374,110 +266,19 @@
 			// ambiledit ------------------------------------------------------------------
 			case 'ambiledit':
 				switch ($_POST['subaksi']) {
-					case 'ju';
-						$s = 'SELECT * FROM '.$tb.'  WHERE replid='.$_POST['replid'];
-						// var_dump($s);exit();
+
+					case 'barkode';
+						$s ='SELECT nilai
+								FROM pus_setting
+								WHERE 
+									kunci="bkfmt"';
+						// print_r($s);exit();
 						$e 		= mysql_query($s);
 						$r 		= mysql_fetch_assoc($e);
 						$stat 	= ($e)?'sukses':'gagal';
 						$out 	= json_encode(array(
 									'status' =>$stat,
-									'datax'  =>array(
-										'nomer'   =>$r['nomer'],
-										'tanggal' =>$r['tanggal'],
-										'uraian'  =>$r['uraian']
-								)));					
-					break;
-
-					case 'katalog';
-						$s = '	SELECT
-									k.kode,
-									k.nama,
-									k.jenis,
-									k.photo2,
-									k.susut,
-									k.keterangan,
-									l.nama as lokasi, 
-									g.nama as grup
-								FROM 
-									'.$tb3.' k,
-									 '.$tb2.' l,
-									 '.$tb.' g
-								WHERE 
-									g.replid = k.grup and 
-									l.replid = g.lokasi and 
-									k.replid ='.$_POST['replid'];
-						$e 		= mysql_query($s);
-						$r 		= mysql_fetch_assoc($e);
-						$stat 	= ($e)?'sukses':'gagal';
-						if(!$e){
-							$stat ='gagal';
-						}else{
-							$stat ='sukses';
-							$dt   =array(
-										'kode'       =>$r['kode'],
-										'nama'       =>$r['nama'],
-										'susut'      =>$r['susut'],
-										'lokasi'     =>$r['lokasi'],
-										'grup'       =>$r['grup'],
-										'photo2'     =>$r['photo2'],
-										'jenis'      =>$r['jenis'],
-										'keterangan' =>$r['keterangan']
-									);						
-						}$out 	= json_encode(array(
-									'status' =>$stat,
-									'data'   =>$dt
-								));					
-					break;
-
-					case 'barang';
-						$s ='SELECT
-								b.tempat,
-								LPAD(b.urut,5,0) as barkode,(
-									SELECT 
-										CONCAT(ll.kode,"/",gg.kode,"/",tt.kode,"/",kk.kode,"/",LPAD(b.urut,5,0))
-									from 
-										sar_katalog kk,
-										sar_grup gg,
-										sar_tempat tt,
-										sar_lokasi ll
-									where 
-										kk.replid = b.katalog AND
-										kk.grup   = gg.replid AND
-										b.tempat  = tt.replid AND
-										tt.lokasi = ll.replid
-								)as kode,
-								b.harga,
-								b.urut,
-								b.kondisi,
-								b.sumber,
-								b.keterangan
-							FROM
-								sar_barang b, sar_kondisi k
-							WHERE
-								b.kondisi = k.replid and
-								b.replid  = '.$_POST['replid'];
-						// print_r($s);exit();
-						$e 		= mysql_query($s);
-						$r 		= mysql_fetch_assoc($e);
-						$stat 	= ($e)?'sukses':'gagal';
-						if(!$e){
-							$stat ='gagal';
-						}else{
-							$stat ='sukses';
-							$dt   =array(
-										'tempat'     =>$r['tempat'],
-										'barkode'    =>$r['barkode'],
-										'urut'       =>$r['urut'],
-										'kode'       =>$r['kode'],
-										'harga'      =>$r['harga'],
-										'kondisi'    =>$r['kondisi'],
-										'sumber'     =>$r['sumber'],
-										'keterangan' =>$r['keterangan']
-									);						
-						}$out 	= json_encode(array(
-									'status' =>$stat,
-									'data'   =>$dt
+									'barkode'   =>$r['nilai']
 								));					
 					break;
 				}
