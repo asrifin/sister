30b7007547ee3cdde5ff7cd75ec025beb9d7d7f3
epiii <gue.elfrianto@gmail.com>
2015-06-13 05:25:50 +0700

    fixing problem

--- purchaseorder/mod/lihatanggaran/admin/admin_lihatanggaran.php
+++ purchaseorder/mod/lihatanggaran/admin/admin_lihatanggaran.php
@@ -42,9 +42,6 @@ $admin .= '
 		<td>Kategori Anggaran</td>
 		<td>:</td>
 		<td>'.getkategorianggaran($kategorianggaran).'</td>
-<<<<<<< HEAD
-			<td>'.getKatAnggaran($kategorianggaran,'kuotaNum').'</td>
-=======
 	</tr>';
 $admin .= '
 	<tr>
@@ -63,7 +60,6 @@ $admin .= '
 		<td>Sisa Anggaran</td>
 		<td>:</td>
 		<td>'.rupiah_format(getKatAnggaran($kategorianggaran,sisaNum)).'</td>
->>>>>>> 0abdc36c6520cd6bb1028bc7153dbba302df179e
 	</tr>';
 $admin .= '</form></table></div>';	
 	}
