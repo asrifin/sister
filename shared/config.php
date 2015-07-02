b3c2bf5dd9e744ea083a8b9c649a41366061f8d8
asrifin <asrifin_ilham@yahoo.com>
2015-07-02 07:33:52 +0700

    siadu akademik

--- shared/config.php
+++ shared/config.php
@@ -1,24 +1,36 @@
-<?php
-//session_start();
-error_reporting(0);
+	<?php
+	error_reporting(0);
 
-date_default_timezone_set('Asia/Jakarta');
-define('ROOTLNK','../');
-define('ROOTDIR','D:/xampp/htdocs/sister/');
-define('SHAREDDIR',ROOTDIR.'shared/');
-define('SHAREDLIB',ROOTDIR.'shared/libraries/');
-define('SHAREDSTYLE',ROOTDIR.'shared/style/');
-define('SHAREDAPPS',ROOTDIR.'shared/apps/');
-define('SHAREDMAINSTYLE',ROOTDIR.'shared/style/main.php');
-define('SHAREDFW',ROOTDIR.'shared/framework.php');
-define('DBFILE',ROOTDIR.'shared/db.php');
-define('SHAREDOBJ',SHAREDLIB.'obj/');
+	date_default_timezone_set('Asia/Jakarta');
 
-define('DB_HRD','`vita`.`hrd_pegawai`');
-define('DB_PUS_SO','`vita`.`hrd_pegawai`');
+	define('ROOTLNK','../');
+	// define('ROOTDIR','/xampp/htdocs/backup/siadu/');
+	define('ROOTDIR','/xampp/htdocs/siadu/');
 
-define('DBHOST','localhost');
-define('DBNAME','sister_siadu');
-define('DBUSER','root');
-define('DBPSWD','');
-?>
+	define('SHAREDDIR',ROOTDIR.'shared/');
+
+	define('SHAREDLIB',ROOTDIR.'shared/libraries/');
+
+	define('SHAREDSTYLE',ROOTDIR.'shared/style/');
+
+	define('SHAREDAPPS',ROOTDIR.'shared/apps/');
+
+	define('SHAREDMAINSTYLE',ROOTDIR.'shared/style/main.php');
+
+	define('SHAREDFW',ROOTDIR.'shared/framework.php');
+
+	define('DBFILE',ROOTDIR.'shared/db.php');
+	define('SHAREDOBJ',SHAREDLIB.'obj/');
+
+
+	// define('DB_HRD','`sister_siadu`.`hrd_pegawai`');
+	// define('DB_HRD','sister_siadu');
+	// define('DB_HRD','sister_siadu');
+
+
+	/* Database */
+
+	define('DBHOST','localhost');
+	define('DBUSER','root');
+	define('DBPSWD','');
+	?>
